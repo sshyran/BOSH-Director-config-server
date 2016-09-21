6635b207fa97eab84c3a2bcc7b2c7f46930efe95
Pivotal <pivotal@dx224.tor.pivotallabs.com>
2016-09-21 16:22:04 -0400

    asd

--- ci/tasks/bosh-release.sh
+++ ci/tasks/bosh-release.sh
@@ -6,13 +6,7 @@ gem install bosh_cli --no-ri --no-rdoc
 cd config-server
 bosh create release --force --with-tarball --name config-server --version acceptance
 
-ls -al ../
-echo "====================================="
-ls -al /
-
-cd ..
-cp config-server/dev_releases/config-server/config-server-acceptance.tgz bosh-release/
-
-ls -al bosh-release
-
-
+#cd ..
+#cp config-server/dev_releases/config-server/config-server-acceptance.tgz bosh-release/
+#
+#ls -al bosh-release
